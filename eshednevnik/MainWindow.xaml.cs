d8efc22aad8185cabc62a9e6683d744cff342fad
heilviva <ep77777777@yandex.ru>
2024-02-22 11:37:59 +0300

    Добавьте файлы проекта.

---
+++ eshednevnik/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class Note
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+}
+
+public class DataSerializer
+{
+    public static void Serialize<T>(T data, string filePath)
+    {
+        string jsonData = JsonConvert.SerializeObject(data);
+        File.WriteAllText(filePath, jsonData);
+    }
+
+    public static T Deserialize<T>(string filePath)
+    {
+        string jsonData = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<T>(jsonData);
+    }
+}
